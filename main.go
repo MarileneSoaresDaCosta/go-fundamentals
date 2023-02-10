66bf45b3958f95f8b0fcd88d7ce0e6860183d86c
Marilene Soares da Costa <cmarilene@vmware.com>
2023-02-09 17:38:29 -0700

    adds upt to session 8

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"demo/menu"
 	"fmt"
+	"os"
 	"strings"
 )
+
+var in = bufio.NewReader(os.Stdin)
 
 func main() {
 	// fmt.Println("Hello, Gophers!")
@@ -96,38 +101,118 @@ func main() {
 	* Struct
 	 */
 
-	type rectangle struct {
-		length float64
-		width  float64
-	}
+	// type rectangle struct {
+	// 	length float64
+	// 	width  float64
+	// }
 
-	fmt.Println(rectangle{10.5, 21.3})
+	// fmt.Println(rectangle{10.5, 21.3})
 
-	var rec rectangle
-	rec.length = 1.3
-	rec.width = 6.9
-	fmt.Println(rec)
+	// var rec rectangle
+	// rec.length = 1.3
+	// rec.width = 6.9
+	// fmt.Println(rec)
 
-	type menuItem struct {
-		name   string
-		prices map[string]float64
-	}
-	// menu is a slice of menuItems - a slice of structs
-	menu := []menuItem{
-		{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
-		{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
-	}
-	for _, item := range menu {
-		fmt.Println(item.name)
-		fmt.Println(strings.Repeat("-", 10))
-		for size, price := range item.prices {
-			fmt.Printf("\t%10s%10.2f\n", size, price)
+	/* if statements */
+	// i := 4
+	// if i < 5 {
+	// 	fmt.Println("i is less than 5")
+	// } else if i < 10 {
+	// 	fmt.Println("i is NOT less than 5 but less than 10")
+	// } else {
+	// 	fmt.Println("i is at least 10")
+	// }
+	// fmt.Println("after the if statement")
+
+	/* switch */
+
+	/* logical switch */
+	// switch i := 8; {
+	// case i < 5:
+	// 	fmt.Println("i is less than 5")
+	// case i < 10:
+	// 	fmt.Println("i is less than 10")
+	// default:
+	// 	fmt.Println("i is greater than 10")
+	// }
+
+	/* Deferred Functions
+	 * we expect to see main 1, main 2, defer 2 and defer 1 in the output
+	 * because the deferred functions are executed in FIRST IN LAST OUT fashion
+	 * this is important when we need to 'release resources', such as closing a db
+	 * connection.
+	 */
+	// fmt.Println("main 1")
+	// defer fmt.Println("defer 1")
+	// fmt.Println("main 2")
+	// defer fmt.Println("defer 2")
+
+	/* Panic and Recover */
+	// dividend, divisor := 10, 5
+
+	// fmt.Printf("%v divided by %v is %v\n", dividend, divisor, divide(dividend, divisor))
+	// // inducing panic >> panic: runtime error: integer divide by zero
+	// dividend, divisor = 10, 0
+	// fmt.Printf("%v divided by %v is %v\n", dividend, divisor, divide(dividend, divisor))
+
+	// 	result, ok := divide2(1, 2)
+	// 	if ok {
+	// 		fmt.Println(result)
+	// 	}
+
+	// 	result, ok = divide2(1, 0)
+	// 	if ok {
+	// 		fmt.Println(result)
+	// 	}
+
+	/* our menu app */
+
+	// a label loop
+loop:
+	for {
+		fmt.Println("Please select an option")
+		fmt.Println(" 1) Print menu")
+		fmt.Println(" 2) Add item")
+		fmt.Println(" q) Quit")
+		choice, _ := in.ReadString('\n')
+
+		switch strings.TrimSpace(choice) {
+		case "1":
+			menu.Print()
+		case "2":
+			menu.AddItem()
+		case "q":
+			break loop
+		default:
+			fmt.Println("Unknown option")
 		}
 	}
-
 }
 
-// part of the 'web service'
+// /* function used in the panic and recover example */
+// func divide(dividend, divisor int) int {
+// 	defer func() {
+// 		// condition avoids nil being printed when there is no panic
+// 		// but, at this point, we get runtime error: integer divide by zero
+// 		// and "10 divided by 0 is 0", which is obviously wrong.
+// 		// we'll find out how to fix this in the next module
+// 		if msg := recover(); msg != nil {
+// 			fmt.Println(msg)
+// 		}
+// 	}()
+// 	return dividend / divisor
+// }
+
+// func divide2(l, r int) (int, bool) {
+// 	if r == 0 {
+// 		return 0, false
+// 	}
+// 	return l / r, true
+// }
+
+/*
+*  part of the 'web service'
+ */
 // func Handler(w http.ResponseWriter, r *http.Request) {
 // 	f, _ := os.Open("./menu.txt")
 // 	io.Copy(w, f)
